d1185f62c77de02761940317258d7dfb6404b041
Naoki Morita <namorit@gmail.com>
2015-07-05 22:02:48 +0900

    Add simple method

--- lib/multilingual/cli.rb
+++ lib/multilingual/cli.rb
@@ -3,13 +3,28 @@ require 'thor'
 
 module Multilingual
   class CLI < Thor
+    class_option :verbose, :type => :boolean
 
-    desc "red WORD", "red word print"
-    option :from
+    desc "hello", "output hello"
+    option :from, :aliases => "-f"
     def hello(name)
+      p "Output hello" if options[:verbose]
       p "From #{options[:from]}" if options[:from]
       p "Hello #{name}"
+      p "End output hello" if options[:verbose]
     end
+
+    desc "update", "update Multilingual swift file"
+    option :destination, :aliases => "-d", :required => true
+    def update
+      p "update #{options[:destination]}"
+    end
+
+    # desc "create", "create Multilingual swift file with given path"
+    # option :destination, :aliases => "-d", :required => true
+    # def create
+    #   invoke :update
+    # end
 
   end
 end
