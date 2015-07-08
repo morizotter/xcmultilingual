5652b10a142800da1f70c3bbfc158a127caef23e
Naoki Morita <namorit@gmail.com>
2015-07-09 01:06:44 +0900

    Add no file message

--- lib/xcmultilingual/writer.rb
+++ lib/xcmultilingual/writer.rb
@@ -6,6 +6,13 @@ module Xcmultilingual
     end
 
     def write()
+      puts "+ UPDATING FILES" if @verbose
+
+      if !File.exist?("#{@destination}")
+        puts "  There is no destination file."
+        return
+      end
+
       File.open("#{@destination}", "r+") do |file|
         file.write("That is the text.")
       end
