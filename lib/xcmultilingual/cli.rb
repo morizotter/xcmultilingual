f95feb603f956a4a342a8fc07924ad3c2e772be8
Naoki Morita <namorit@gmail.com>
2015-07-12 18:22:05 +0900

    Add description to options

--- lib/xcmultilingual/cli.rb
+++ lib/xcmultilingual/cli.rb
@@ -7,15 +7,15 @@ module Xcmultilingual
     class_option :verbose, :type => :boolean
 
     desc "update", "update xcmultilingual swift file"
-    option :destination, :aliases => "-d", :required => true
-    option :name, :aliases => "-n"
+    option :destination, :aliases => "-d", :required => true, :desc => "generating file destination"
+    option :name, :aliases => "-n", :default => "Multilingual", :desc => "Localization struct name"
     def update
       parser = Parser.new(options[:destination])
       parser.verbose = options[:verbose] || false
       bundle_data = parser.parse
 
       writer = Writer.new(options[:destination], bundle_data)
-      writer.name = options[:name] || "Multilingual"
+      writer.name = options[:name]
       writer.verbose = options[:verbose] || false
       writer.write
 
