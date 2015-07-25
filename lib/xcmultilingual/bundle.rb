c72e0e143759cf64f365203182e353e36b24354f
Naoki Morita <namorit@gmail.com>
2015-07-26 00:20:23 +0900

    Remove relative path

--- lib/xcmultilingual/bundle.rb
+++ lib/xcmultilingual/bundle.rb
@@ -1,10 +1,9 @@
 module Xcmultilingual
-  Bundle = Struct.new("Bundle", :relative_path, :name, :tables) do
+  Bundle = Struct.new("Bundle", :name, :tables) do
     def description
       name_description = name.nil? ? "MAIN BUNDLE" : name
-      relative_path_description = relative_path.nil? ? "NO RELATIVE PATH" : relative_path
       tables_description = tables.map { |table| table.description }.join(" / ")
-      "[#{name_description}]\nname: #{name_description}, relative_path: #{relative_path_description}, tables: #{tables_description}"
+      "[#{name_description}]\nname: #{name_description}, tables: #{tables_description}"
     end
   end
 
